e72fa4310615ba3de571214e20db382b3f0f42e3
Lazhari <lazhari.mohammed@outlook.com>
2020-07-19 02:53:36 +0200

    Created a sample example for enums

---
+++ enums/enum_demo.py
@@ -0,0 +1,19 @@
+import enums.enum_example_pb2 as enum_example_pb2
+
+enum_message = enum_example_pb2.EnumMessage()
+
+enum_message.id = 1
+enum_message.day_of_the_week = enum_example_pb2.MONDAY
+
+print(enum_message.day_of_the_week == enum_example_pb2.MONDAY)
+
+with open("enums.bin", "wb") as f:
+    f.write(enum_message.SerializeToString())
+    print("Wrote to a file")
+
+with open("enums.bin", "rb") as f:
+    enum_message_read = enum_example_pb2.EnumMessage().FromString(f.read())
+    print("read from file")
+    print(enum_message_read)
+
+print(enum_message)
